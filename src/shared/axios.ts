7514692886337f97e152a9f4236eb22e0c7b40af
somashekarp <somasekhar.puralasetti@ateliasoftware.com>
2024-08-15 12:50:56 +0530

    chore: Update package.json to include "https" dependency

--- src/shared/axios.ts
+++ src/shared/axios.ts
@@ -3,8 +3,5 @@ import axios from "axios";
 
 export const http = axios.create({
 	baseURL: environment.baseUrl,
-	maxRedirects: 0,  // Prevent following redirects to HTTPS
-    // Optionally, add timeout and other settings if needed
-    timeout: 10000, // for example, set a timeout of 10 seconds
 
 });
