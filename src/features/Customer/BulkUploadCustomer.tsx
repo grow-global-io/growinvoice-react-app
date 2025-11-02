ba3f38577eb821c09cf9f4afe500abdc7b5f56b2
somashekar <somasekhar.puralasetti@ateliasoftware.com>
2025-11-02 16:49:18 +0530

    fix: remove unused import of Country from phone number input library

--- src/features/Customer/BulkUploadCustomer.tsx
+++ src/features/Customer/BulkUploadCustomer.tsx
@@ -2,7 +2,7 @@ import { Box, Button, Grid, Tooltip, Typography } from "@mui/material";
 import { RegexExp } from "@shared/regex";
 import { useTranslation } from "react-i18next";
 import * as Yup from "yup";
-import { formatPhoneNumber, isValidPhoneNumber, type Country } from "react-phone-number-input";
+import { formatPhoneNumber, isValidPhoneNumber } from "react-phone-number-input";
 import {
 	type CreateCustomerWithAddressDto,
 	CreateCustomerWithAddressDtoOption,
