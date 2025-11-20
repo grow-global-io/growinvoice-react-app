b7989d8fdebe6d8c93083015039c94529e7acbcb
Souvik Basu <bsouvik986.com>
2025-11-20 16:36:59 +0530

    updated

--- src/features/Customer/CustomersInvoicesSections.tsx
+++ src/features/Customer/CustomersInvoicesSections.tsx
@@ -24,7 +24,7 @@ const CustomersInvoicesSections = ({ customerId }: { customerId: string }) => {
 		if (!customerId) return;
 		navigate(`/invoice/createinvoice?customerId=${customerId}`);
 	};
-	//test comment
+	//test comment 1
 	const handleCreateReceipt = () => {
 		if (!customerId) return;
 		navigate(`/receipt/createreceipt?customerId=${customerId}`);
