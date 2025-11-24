ba32dd12175e95c724173fd9431fb78715347b55
Soumyajit2000-web <soumyajitd2000@gmail.com>
2025-11-24 14:40:19 +0530

    chore: remove local config

--- vite.config.ts
+++ vite.config.ts
@@ -108,7 +108,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			"/api": {
-				target: process.env.VITE_BASE_URL || "http://localhost:5001",
+				target: process.env.VITE_BASE_URL,
 				changeOrigin: true,
 				secure: false,
 				rewrite: (path) => path,
